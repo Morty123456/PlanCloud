29f6c9907a944e0a61f92fee13dc825f72823aaf
Morty123456 <13021151355@163.com>
2020-08-29 18:38:33 +0800

    更新

--- hystrix/src/main/resources/static/actionJs/fileUpLoad.js
+++ hystrix/src/main/resources/static/actionJs/fileUpLoad.js
@@ -3,11 +3,13 @@ $(document).ready(function () {
     file = $('<input type="file" />');
     $("#searchFile").click(function() {
         // alert("上传文件");
+        // alert(file);
         // $("#file").trigger('click');
         file.click();
     })
     file.change(function(e){
         var select_file = file[0].files[0].name;
+        //前端文件改名
         $("#fileName").val(select_file);
         // alert(select_file);
     })
@@ -16,6 +18,10 @@ $(document).ready(function () {
         var formData = new FormData();
         formData.append('file', file[0].files[0]);
         formData.append('fileType', fileType);
+        //打印file的信息瞅一眼
+        console.log(file);
+        console.log(file[0].files[0]);
+
         alert(file[0].files[0].name);
         $.ajax({
             url:"/file",
